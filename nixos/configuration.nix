5be5fbf962bff23d3da940e5bc669e530a5aa85c
Justin Blake <justin@blaix.com>
2023-07-24 19:27:34 -0400

    update home-manager

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,7 +7,7 @@
 with lib;
 
 let 
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz"; 
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz"; 
 in
 {
   imports =
