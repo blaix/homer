a7315f693c214b707ee4847c7f416000a56b00d3
Justin Blake <justin@blaix.com>
2023-09-27 14:08:32 -0400

    handle spaces in filenames in fv alias

--- shared/home.nix
+++ shared/home.nix
@@ -255,7 +255,7 @@ in
     shellAliases = {
       cat = "bat";
       vim = "nvim";
-      fv = "nvim $(fzf)";
+      fv = "nvim \"$(fzf)\"";
       t = "tasker";
       wiki = "cd ~/Sync/wiki";
       homer = "cd ~/homer";
