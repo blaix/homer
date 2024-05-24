8823e7a6b39a732e2f93ddcafee706b565c7bd8b
Justin Blake <justin@blaix.com>
2024-05-24 06:15:17 -0400

    keep orb nix vm unique and using normal config

--- hosts/nixos/common.nix
+++ hosts/nixos/common.nix
@@ -1,12 +1,9 @@
 { pkgs, ... }:
 {
-  imports = [ ../common.nix ];
-  
   # ---------------------------------------------------------------------------
   #   Settings shared among all my NixOs systems
   # ---------------------------------------------------------------------------
   
-  # create a group to match the default on mac
-  users.groups.staff = {};
-
+  imports = [ ../common.nix ];
+  
 }
