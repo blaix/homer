f0e5b89f455691ed3fdd604f338c99e3d44e84d3
Justin Blake <justin@blaix.com>
2024-10-15 23:08:16 -0400

    upgrade to 24.05

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Blaix Flakes";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.url = "github:LnL7/nix-darwin";
