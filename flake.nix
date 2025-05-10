67c4dc0308ce1916972976fd978aee1419355b6f
Justin Blake <justin@blaix.com>
2025-05-10 18:28:31 -0400

    switch to lix!

--- flake.nix
+++ flake.nix
@@ -7,15 +7,20 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.url = "github:LnL7/nix-darwin";
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, lix-module, ... }: {
 
     # macs
     darwinConfigurations = {
       arwen = nix-darwin.lib.darwinSystem {
         system = "aarch64-darwin";
         modules = [ 
+          lix-module.nixosModules.default
           ./hosts/mac/arwen.nix
           home-manager.darwinModules.home-manager {
             home-manager.users.justin = import ./home.nix;
@@ -25,6 +30,7 @@
       bilbo = nix-darwin.lib.darwinSystem {
         system = "aarch64-darwin";
         modules = [ 
+          lix-module.nixosModules.default
           ./hosts/mac/bilbo.nix
           home-manager.darwinModules.home-manager {
             home-manager.users.justin = import ./home.nix;
@@ -38,6 +44,7 @@
       orb = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         modules = [ 
+          lix-module.nixosModules.default
           ./hosts/nixos/orb.nix
           home-manager.nixosModules.home-manager {
             home-manager.users.justin = import ./home.nix;
