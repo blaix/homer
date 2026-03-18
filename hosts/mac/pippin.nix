61bc9154c2ac5554604aa0157734cfb30e465e56
Justin Blake <justin@blaix.com>
2026-03-18 13:18:07 -0400

    restart pippin(ix) after power failure

--- hosts/mac/pippin.nix
+++ hosts/mac/pippin.nix
@@ -10,6 +10,9 @@
   # using a windows keyboard on pippin
   system.keyboard.swapLeftCommandAndLeftAlt = true;
 
+  # Restart after power failure so NixOS comes back up on the dual-boot
+  power.restartAfterPowerFailure = true;
+
   # match the GID that Nix was installed with on this machine
   ids.gids.nixbld = 350;
 }
